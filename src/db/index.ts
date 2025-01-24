4ed1575ce0b067e32b699a86e6f196277976d904
Bojan Stankovic <bstankov1160@gmail.com>
2025-01-24 12:00:45 +0100

    db conn

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,35 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
-const connectionString = process.env.DATABASE_URL!;
+// const connectionString = process.env.DATABASE_URL!;
+// Extract database connection details from the DATABASE_URL
+const dbUrl = process.env.DATABASE_URL!;
+if (!dbUrl) {
+  throw new Error('DATABASE_URL not set');
+}
+
+const { host, port, user, password, database } = parseDbUrl(dbUrl);
+
 const pool = new Pool({
-  connectionString,
+  host,
+  port: parseInt(port),
+  user,
+  password,
+  database,
   ssl: true,
 });
 
 export const db = drizzle(pool);
+
+function parseDbUrl(dbUrl: string) {
+  const { username, password, hostname, port, pathname } = new URL(dbUrl);
+  const database = pathname.slice(1); // remove leading "/"
+
+  return {
+    host: hostname,
+    port: port,
+    user: username,
+    password: password,
+    database,
+  };
+}
